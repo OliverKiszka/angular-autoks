400bd523695e2ac5347e1b5763f50c392bbfe0e6
OliverKiszka <ko41607@zut.edu.pl>
2020-01-17 23:54:29 +0100

    1 file modified

--- src/app/wydatki.service.ts
+++ src/app/wydatki.service.ts
@@ -1,21 +1,40 @@
-import { Injectable } from '@angular/core';
-import { Wydatek, KATEGORIE } from './wydatek';
+import { Injectable } from "@angular/core";
+import { Wydatek, KATEGORIE } from "./wydatek";
 
 @Injectable()
 export class WydatkiService {
-  
-
   private wydatki: Wydatek[];
   private nextId: number;
 
-  constructor() { 
-   this.wydatki = [
-      new Wydatek(1, 'Tankowanie', 252.40, '', new Date(2018, 4, 30), 527.5, 48),
-      new Wydatek(2, 'Myjnia', 75, '', new Date(2018, 4, 2)),
-      new Wydatek(3, 'Serwis', 1250, 'Klocki, rozrząd i parę drobiazgów', new Date(2018, 3, 16)),
-      new Wydatek(4, 'Inne', 315.50, 'Różowe futerko na kierownicę', new Date(2018, 3, 16),55,55),
-      new Wydatek(5, 'Tankowanie', 325.20, 'Do pełna po urlopie', new Date(2017, 7, 12), 527),
-      new Wydatek(6, 'Tankowanie', 257.30, '', new Date(2017, 7, 13), null, 35)
+  constructor() {
+    this.wydatki = [
+      new Wydatek(1, "Tankowanie", 252.4, "", new Date(2018, 4, 30), 527.5, 48),
+      new Wydatek(2, "Myjnia", 75, "", new Date(2018, 4, 2)),
+      new Wydatek(
+        3,
+        "Serwis",
+        1250,
+        "Klocki, rozrząd i parę drobiazgów",
+        new Date(2018, 3, 16)
+      ),
+      new Wydatek(
+        4,
+        "Inne",
+        315.5,
+        "Różowe futerko na kierownicę",
+        new Date(2018, 3, 16),
+        55,
+        55
+      ),
+      new Wydatek(
+        5,
+        "Tankowanie",
+        325.2,
+        "Do pełna po urlopie",
+        new Date(2017, 7, 12),
+        527
+      ),
+      new Wydatek(6, "Tankowanie", 257.3, "", new Date(2017, 7, 13), null, 35)
     ];
     this.nextId = 7;
   }
@@ -26,6 +45,7 @@ export class WydatkiService {
     return KATEGORIE;
   }
   dodajWydatek(nowyWydatek: Wydatek): void {
+    nowyWydatek.data = new Date(nowyWydatek.data);
     this.wydatki.push(nowyWydatek);
     nowyWydatek.id = this.nextId++;
   }
@@ -33,5 +53,4 @@ export class WydatkiService {
     const ind = this.wydatki.findIndex(wydatek => wydatek.id === id);
     this.wydatki.splice(ind, 1);
   }
-  
 }
